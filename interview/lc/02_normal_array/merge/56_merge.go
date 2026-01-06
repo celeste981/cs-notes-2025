d19c97cd44a2d9dae497c073e17b46676b24b739
celeste981 <chen_199811@qq.com>
2026-01-06 17:19:10 +0800

    lc

--- interview/lc/02_normal_array/merge/56_merge.go
+++ interview/lc/02_normal_array/merge/56_merge.go
@@ -1,5 +1,32 @@
 package merge
 
+import (
+	"sort"
+)
+
 func Merge(intervals [][]int) [][]int {
-	return intervals
+	// 按左边数字来排序
+	//for _, interval := range intervals {
+	//	sort.Ints(interval)
+	//}
+	sort.Slice(intervals, func(i, j int) bool {
+		a, b := intervals[i], intervals[j]
+		return a[0] < b[0]
+	})
+
+	ans := make([][]int, 0)
+	cur := intervals[0]
+	for _, interval := range intervals {
+		if interval[0] <= cur[1] {
+			if interval[1] > cur[1] {
+				cur[1] = interval[1]
+			}
+		} else {
+			ans = append(ans, cur)
+			cur = interval
+		}
+	}
+	ans = append(ans, cur)
+
+	return ans
 }
--- interview/lc/02_normal_array/merge/56_merge_test.go
+++ interview/lc/02_normal_array/merge/56_merge_test.go
@@ -2,6 +2,7 @@ package merge
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -11,16 +12,35 @@ func TestMerge(t *testing.T) {
 		args [][]int
 		want [][]int
 	}{
-		{},
-		{},
-		{},
+		{"basic", [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}, [][]int{{1, 6}, {8, 10}, {15, 18}}},
+		{"reorder", [][]int{{4, 7}, {1, 4}}, [][]int{{1, 7}}},
+		{"no_interval", [][]int{{4, 7}, {1, 3}}, [][]int{{4, 7}, {1, 3}}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Merge(tt.args); !reflect.DeepEqual(got, tt.want) {
+			got := Merge(tt.args)
+			normalize(got)
+			normalize(tt.want)
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Merge(%v) = %v, want %v", tt.args, got, tt.want)
 			}
 		})
 	}
+}
+
+func normalize(args [][]int) [][]int {
+	for _, arg := range args {
+		sort.Ints(arg)
+	}
+	sort.Slice(args, func(i, j int) bool {
+		a, b := args[i], args[j]
+		for k := 0; k < len(a) && k < len(b); k++ {
+			if a[k] != b[k] {
+				return a[k] < b[k]
+			}
+		}
+		return len(a) < len(b)
+	})
+	return args
 }
